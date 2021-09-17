77a6fb3d9207f10203eab5f7de6cfe2b77b2a0c1
효주 이 <gywn332@nate.com>
2021-09-17 18:20:12 +0900

    초기 커밋.

---
+++ gitgitgitest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitgitgitest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("깃허브 테스트");
+        }
+    }
+}
